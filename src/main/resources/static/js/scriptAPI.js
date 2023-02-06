8c0df9986edb5fca3b1dc965764b063c267bd70f
Mañana <jonathandmgr07@gmail.com>
2023-02-06 12:24:43 +0100

    funcionando el feed con API

--- src/main/resources/static/js/scriptAPI.js
+++ src/main/resources/static/js/scriptAPI.js
@@ -1,11 +1,17 @@
+const http = new XMLHttpRequest
+const url = "https://newsapi.org/v2/everything?q=DGT&apiKey=48108a4dda8a47538c4a2ae5503dd6b4"
+http.open("GET", url)
+http.send()
+
 let body=document.querySelectorAll("body")[0]
 let articleDiv=document.createElement("div")
 articleDiv.setAttribute("id","news")
-fetch("https://newsapi.org/v2/everything?q=DGT&apiKey=48108a4dda8a47538c4a2ae5503dd6b4")
-  .then(response => response.json())
-  .then(data => {
-    data.articles.forEach(article => {
-      let articleItem=document.createElement("div")
+body.appendChild(articleDiv)
+
+http.addEventListener('load', (data) => { //para cada usuario
+    const dataJSON = JSON.parse(data.target.response)
+        for (let article of dataJSON.articles) {
+            let articleItem=document.createElement("div")
       articleItem.setAttribute("class","card article")
       let articleImg=document.createElement("img")
       articleImg.setAttribute("src",article.urlToImage)
@@ -25,9 +31,6 @@ fetch("https://newsapi.org/v2/everything?q=DGT&apiKey=48108a4dda8a47538c4a2ae550
       articleItem.appendChild(articleContent)
       articleItem.style.marginTop="5px"
       articleDiv.appendChild(articleItem)
-    });
-    body.appendChild(articleDiv)
-  })
-  .catch(error => {
-    console.error(error);
-  });
+        }
+   
+})
