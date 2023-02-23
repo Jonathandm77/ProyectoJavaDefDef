3cadcae39334d855679e6c6229c6f273d9340e65
Mañana <jonathandmgr07@gmail.com>
2023-02-23 09:50:11 +0100

    mas cambios

--- src/main/java/principal/persistencia/Tablas.java
+++ src/main/java/principal/persistencia/Tablas.java
@@ -12,42 +12,32 @@ import principal.modelo.Usuario;
 
 public class Tablas {
 
-	public void crearTablas() throws ParseException {
-
-		Alumno a1 = new Alumno("Alba", "541245F");
-		Profesor p1 = new Profesor("Pedro", "4529L");
-		Coche c1 = new Coche("5894 GHL", "2023", "Peugeot");
-		Rol rolBasico = new Rol("ROLE_USER");
-		Rol rolAdmin = new Rol("ROLE_ADMIN");
-		Usuario usuarioAdmin = new Usuario("admin", "admin", "$2a$12$jcd/P/f86nT9YXQbx/3tJeQye8h0aMhNm6EzzKkZMNLUCtIxpTNJa");
-		usuarioAdmin.getRoles().add(rolAdmin);
-
-		/*
-		 * LocalDate fecha=LocalDate.of(2024, 5, 13); c1.setFechaITV(fecha);
-		 */
-		Date fecha = new Date(125, 6, 23);
-		c1.setFechaITV(fecha);
-
-		a1.setProfesor(p1);
-		p1.getAlumnos().add(a1);
-		a1.setCoche(c1);
-		c1.getAlumnos().add(a1);
-		p1.getCoches().add(c1);
-		c1.getProfesores().add(p1);
-
-		RolDAO rolDAO = new RolDAO();
-		UsuarioDAO usuarioDAO = new UsuarioDAO();
-		AlumnoDAO alumnoDAO = new AlumnoDAO();
-		ProfesorDAO profesorDAO = new ProfesorDAO();
-		CocheDAO cocheDAO = new CocheDAO();
-		
-		usuarioDAO.insertarUsuarioJPA(usuarioAdmin);
-		rolDAO.insertarRolJPA(rolAdmin);
-		rolDAO.insertarRolJPA(rolBasico);
-		profesorDAO.insertarProfesorJPA(p1);
-		cocheDAO.insertarCocheJPA(c1);
-		alumnoDAO.insertarAlumnoJPA(a1);
-
-	}
-
+	/*
+	 * public void crearTablas() throws ParseException {
+	 * 
+	 * Alumno a1 = new Alumno("Alba", "541245F"); Profesor p1 = new
+	 * Profesor("Pedro", "4529L"); Coche c1 = new Coche("5894 GHL", "2023",
+	 * "Peugeot"); Rol rolBasico = new Rol("ROLE_USER"); Rol rolAdmin = new
+	 * Rol("ROLE_ADMIN"); Usuario usuarioAdmin = new Usuario("admin", "admin",
+	 * "$2a$12$jcd/P/f86nT9YXQbx/3tJeQye8h0aMhNm6EzzKkZMNLUCtIxpTNJa");
+	 * usuarioAdmin.getRoles().add(rolAdmin);
+	 * 
+	 * 
+	 * LocalDate fecha=LocalDate.of(2024, 5, 13); c1.setFechaITV(fecha);
+	 * 
+	 * Date fecha = new Date(125, 6, 23); c1.setFechaITV(fecha);
+	 * 
+	 * a1.setProfesor(p1); p1.getAlumnos().add(a1); a1.setCoche(c1);
+	 * c1.getAlumnos().add(a1); p1.getCoches().add(c1); c1.getProfesores().add(p1);
+	 * 
+	 * RolDAO rolDAO = new RolDAO(); UsuarioDAO usuarioDAO = new UsuarioDAO();
+	 * AlumnoDAO alumnoDAO = new AlumnoDAO(); ProfesorDAO profesorDAO = new
+	 * ProfesorDAO(); CocheDAO cocheDAO = new CocheDAO();
+	 * 
+	 * usuarioDAO.insertarUsuarioJPA(usuarioAdmin); rolDAO.insertarRolJPA(rolAdmin);
+	 * rolDAO.insertarRolJPA(rolBasico); profesorDAO.insertarProfesorJPA(p1);
+	 * cocheDAO.insertarCocheJPA(c1); alumnoDAO.insertarAlumnoJPA(a1);
+	 * 
+	 * }
+	 */
 }
