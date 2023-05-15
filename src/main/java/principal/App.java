e2ee004625de90898aa1663b9e34705a1a03f65a
Jonathan FCT <jonathandmgr07@gmail.com>
2023-05-15 15:25:26 +0200

    se crean clases, mejorar estética y mostrarlas en calendario

--- src/main/java/principal/App.java
+++ src/main/java/principal/App.java
@@ -4,7 +4,6 @@ import java.text.ParseException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class App {
---
+++ src/main/java/principal/controller/CalendarioController.java
@@ -0,0 +1,48 @@
+package principal.controller;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import principal.modelo.Alumno;
+import principal.modelo.Clase;
+import principal.servicio.implementacion.AlumnoServiceImpl;
+import principal.servicio.implementacion.ClaseServiceImpl;
+
+@Controller
+@RequestMapping("/calendario")
+public class CalendarioController {
+	@Autowired
+	AlumnoServiceImpl alumnoService;
+	@Autowired
+	ClaseServiceImpl claseService;
+	
+	@GetMapping({"","/"})
+	String homeCalendar(Model model) {
+		ArrayList<Alumno> listaAlumnos=(ArrayList<Alumno>) alumnoService.listarAlumnos();
+		model.addAttribute("listaAlumnos",listaAlumnos);
+       model.addAttribute("claseNueva",new Clase());
+		return "calendario";
+	}
+	
+	@PostMapping({"/add"})
+	String añadirClase(@ModelAttribute("claseNueva") Clase clase,@RequestParam("fecha") String fecha) throws ParseException {
+		Clase claseAñadir=new Clase(clase.getAlumno());
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		claseAñadir.setFecha(dateFormat.parse(fecha));
+		claseAñadir.setHora(clase.getHora());
+		claseService.insertarClase(claseAñadir);
+		
+		return "redirect:/calendario";
+		
+	}
+}
